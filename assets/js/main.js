2b437401ee33b82f509bf89c6b5ee66042079c5d
azabraao <az.abraao@gmail.com>
2020-06-05 13:21:28 -0300

    Remove the use of navbar and script includes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
-const menu = {
-  init() {
-    $(".jsHamburguer").on("click", menu.handleHamburguerClick);
-    $(".jsMenuDropdown").on("click", menu.handleMenuDropdownClick);
-  },
-  handleMenuDropdownClick(event) {
-    $(this).toggleClass("active");
-  },
-  handleHamburguerClick() {
-    menu.makeHamburguerAnimation();
+$(document).ready(function () {
+  const menu = {
+    init() {
+      $(".jsHamburguer").on("click", menu.handleHamburguerClick);
+      $(".jsMenuDropdown").on("click", menu.handleMenuDropdownClick);
+    },
+    handleMenuDropdownClick(event) {
+      $(this).toggleClass("active");
+    },
+    handleHamburguerClick() {
+      menu.makeHamburguerAnimation();
 
-    $(".jsMenu").toggleClass("active");
-  },
-  makeHamburguerAnimation() {
-    $(".jsHamburguer").addClass("animate");
+      $(".jsMenu").toggleClass("active");
+    },
+    makeHamburguerAnimation() {
+      $(".jsHamburguer").addClass("animate");
 
-    setTimeout(() => {
-      $(".jsHamburguer").removeClass("animate");
-    }, 300);
-  },
-};
-
-menu.init();
+      setTimeout(() => {
+        $(".jsHamburguer").removeClass("animate");
+      }, 300);
+    },
+  };
+  menu.init();
+});
